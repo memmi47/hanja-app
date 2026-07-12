c5172012667ca7eaa1816b23039db41a09502988
memmi47 <memmi47@gmail.com>
2026-07-12 17:59:49 +0900

    한자 학습 PWA 초기 배포
    GitHub Pages로 아이패드/아이폰 홈 화면에 추가해 쓸 수 있는 정적 웹앱.
    학습 기록은 각 기기의 localStorage에만 저장되고 서버에는 아무것도 남지 않는다.

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mhj-cache-v1';
+const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
